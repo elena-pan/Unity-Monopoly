c06c0a7260b0269e1160b971c77c73e70a4d8d7f
Elena P <elena.pan.ep@gmail.com>
2020-11-06 15:32:44 -0700

    Change camera input

--- Monopoly/Assets/Scripts/CameraController.cs
+++ Monopoly/Assets/Scripts/CameraController.cs
@@ -1,76 +1,130 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
  
 public class CameraController : MonoBehaviour {
  
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-     
-     
-    float mainSpeed = 100.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
-    private float totalRun= 1.0f;
-     
-    void Update () {
-        lastMouse = Input.mousePosition - lastMouse ;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse =  Input.mousePosition;
-        //Mouse  camera angle done.  
-       
-        //Keyboard commands
-        float f = 0.0f;
-        Vector3 p = GetBaseInput();
-        if (Input.GetKey (KeyCode.LeftShift)){
-            totalRun += Time.deltaTime;
-            p  = p * totalRun * shiftAdd;
-            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-        }
-        else{
-            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
-        }
-       
-        p = p * Time.deltaTime;
-       Vector3 newPosition = transform.position;
-        if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
-            transform.Translate(p);
-            newPosition.x = transform.position.x;
-            newPosition.z = transform.position.z;
-            transform.position = newPosition;
-        }
-        else{
-            transform.Translate(p);
-        }
-       
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 50F;
+    public float sensitivityY = 50F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -90F;
+    public float maximumY = 90F;
+    float rotationY = -60F;
+
+    void Update()
+    {
+        MouseInput();
     }
-     
-    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
-        Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey (KeyCode.W)){
-            p_Velocity += new Vector3(0, 0 , 1);
+
+    void MouseInput()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
         }
-        if (Input.GetKey (KeyCode.S)){
-            p_Velocity += new Vector3(0, 0, -1);
+
+        if (Input.GetMouseButton(0))
+        {
         }
-        if (Input.GetKey (KeyCode.A)){
-            p_Velocity += new Vector3(-1, 0, 0);
+        else if (Input.GetMouseButton(1))
+        {
+            MouseRightClick();
         }
-        if (Input.GetKey (KeyCode.D)){
-            p_Velocity += new Vector3(1, 0, 0);
+        else if (Input.GetMouseButton(2))
+        {
+            MouseMiddleButtonClicked();
         }
-        return p_Velocity;
+        else if (Input.GetMouseButtonUp(1))
+        {
+            ShowAndUnlockCursor();
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            ShowAndUnlockCursor();
+        }
+        else
+        {
+            MouseWheeling();
+        }
+    }
+
+    void ShowAndUnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void HideAndLockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void MouseMiddleButtonClicked()
+    {
+        HideAndLockCursor();
+        Vector3 NewPosition = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+        Vector3 pos = transform.position;
+        if (NewPosition.x > 0.0f)
+        {
+            pos += transform.right;
+        }
+        else if (NewPosition.x < 0.0f)
+        {
+            pos -= transform.right;
+        }
+        if (NewPosition.z > 0.0f)
+        {
+            pos += transform.forward;
+        }
+        if (NewPosition.z < 0.0f)
+        {
+            pos -= transform.forward;
+        }
+        pos.y = transform.position.y;
+        transform.position = pos;
+    }
+
+    void MouseRightClick()
+    {
+        HideAndLockCursor();
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+    void MouseWheeling()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            pos = pos - transform.forward;
+            transform.position = pos;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            pos = pos + transform.forward;
+            transform.position = pos;
+        }
     }
 }
